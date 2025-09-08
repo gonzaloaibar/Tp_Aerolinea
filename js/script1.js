223647230b914b5237c499a57caeb23bee3a27c9
arielgonzalo <aibargonzalo0@gmail.com>
2025-09-08 00:27:33 -0300

    ejercicio3 parcialmente echo

--- js/script1.js
+++ js/script1.js
@@ -18,16 +18,21 @@ function confirmar_fecha_vuelo(fecha){
     
 }
 
+const form1= document.getElementById("formulario");
 
+if(form1){
 
-document.getElementById("formulario").addEventListener("submit", function(event){
-    const fechaIngresada = document.getElementById("fecha").value;
-    const fechaValida = confirmar_fecha_vuelo(fechaIngresada);
+    form1.addEventListener("submit", function(event){
+        const fechaIngresada = document.getElementById("fecha").value;
+        const fechaValida = confirmar_fecha_vuelo(fechaIngresada);
 
-    if(!fechaValida){
-        event.preventDefault();
-    }
-})
+        if(!fechaValida){
+            event.preventDefault();
+        }
+    });
+
+}
+
 
 //Ejercicio 2
 
@@ -54,19 +59,43 @@ class SistemaReserva{
     }
 }
 
-document.getElementById("formulario2").addEventListener("submit", function(event){
-    event.preventDefault();
+const form2= document.getElementById("formulario2");
 
-    const nombre= document.getElementById("nombre").value;
-    const dni= document.getElementById("dni").value;
-    const sexo = document.querySelector('input[name="sexo"]:checked').value;
-    const origen= document.getElementById("origen").value;
-    const destino = document.getElementById("destino").value;
-    const fecha_vuelo= document.getElementById("fecha_vuelo").value;
-    const clase= document.getElementById("clase").value;
-    const ubicacion= document.getElementById("ubicacion").value;
+if(form2){
+    form2.addEventListener("submit", function(event){
+        event.preventDefault();
 
-    console.log(nombre);
+        const nombre= document.getElementById("nombre").value;
+        const dni= document.getElementById("dni").value;
+        const sexo = document.querySelector('input[name="sexo"]:checked').value;
+        const origen= document.getElementById("origen").value;
+        const destino = document.getElementById("destino").value;
+        const fecha_vuelo= document.getElementById("fecha_vuelo").value;
+        const clase= document.getElementById("clase").value;
+        const ubicacion= document.getElementById("ubicacion").value;
 
+        console.log(nombre);
+
+        //referencia a la tabla y su cuerpo
+        const tabla=document.getElementById("tabla").getElementsByTagName("tbody")[0];
+
+        //creo la fila
+        const fila=document.createElement('tr');
+        fila.innerHTML=`
+            <td>${nombre}</td>
+            <td>${dni}</td>
+            <td>${sexo}</td>
+            <td>${origen}</td>
+            <td>${destino}</td>
+            <td>${fecha_vuelo}</td>
+            <td>${clase}</td>
+            <td>${ubicacion}</td>
+        `;
+
+        tabla.appendChild(fila);
+
+        document.getElementById("formulario2").reset();
     
-});
+    });
+}
+
