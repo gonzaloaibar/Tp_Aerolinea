1b996a3d2fbaf031f71b2bbe3a27679d01a02c44
arielgonzalo <aibargonzalo0@gmail.com>
2025-09-07 23:55:22 -0300

    se valido los formularios

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,4 @@
+
 //EJERCICIO 1
 
 function confirmar_fecha_vuelo(fecha){
@@ -17,6 +18,8 @@ function confirmar_fecha_vuelo(fecha){
     
 }
 
+
+
 document.getElementById("formulario").addEventListener("submit", function(event){
     const fechaIngresada = document.getElementById("fecha").value;
     const fechaValida = confirmar_fecha_vuelo(fechaIngresada);
@@ -26,4 +29,44 @@ document.getElementById("formulario").addEventListener("submit", function(event)
     }
 })
 
+//Ejercicio 2
 
+function Reserva(destino, origen, fecha_hora, nombre_pasajero, dni, fec_nacimiento, clase, ubicacion){
+    this.destino=destino;
+    this.origen=origen;
+    this.fecha_hora=fecha_hora;
+    this.nombre_pasajero=nombre_pasajero;
+    this.dni=dni;
+    this.fec_nacimiento=fec_nacimiento;
+    this.clase=clase;
+    this.ubicacion=ubicacion;
+
+
+}
+
+class SistemaReserva{
+    constructor(){
+        this.reservas=[];
+    }
+
+    agregarReserva(reserva){
+        this.reservas.push(reserva);
+    }
+}
+
+document.getElementById("formulario2").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const nombre= document.getElementById("nombre").value;
+    const dni= document.getElementById("dni").value;
+    const sexo = document.querySelector('input[name="sexo"]:checked').value;
+    const origen= document.getElementById("origen").value;
+    const destino = document.getElementById("destino").value;
+    const fecha_vuelo= document.getElementById("fecha_vuelo").value;
+    const clase= document.getElementById("clase").value;
+    const ubicacion= document.getElementById("ubicacion").value;
+
+    console.log(nombre);
+
+    
+});
